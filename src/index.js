7c72b503cdb428491ff02af164f24c422a7d86f4
Alexey <Shaht3r@gmail.com>
2020-02-22 23:57:24 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const tempArr = String(n).split("");
+    const prepareArr = tempArr.filter(item => item !== "-");
+    return prepareArr.reverse().join("");
 }
